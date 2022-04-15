837bbf6147de968f8391787384a7d4bdb2d9f63c
mc-b <marcel.bernet@ch-open.ch>
2022-04-15 08:51:15 +0200

    set primary vm

--- main.tf
+++ main.tf
@@ -6,10 +6,16 @@ resource "null_resource" "multipass" {
   triggers = {
     name = var.module
   }
+  
+  # terraform apply
   provisioner "local-exec" {
     command = "multipass launch --name ${var.module} -c${var.cpu} -m${var.mem} -d${var.disk} --cloud-init ${var.userdata}"
-#   command = "multipass set client.primary-name=${var.module}"
   }
+  provisioner "local-exec" {
+   command = "multipass set client.primary-name=${var.module}"
+  }
+  
+  # terraform destroy
   provisioner "local-exec" {
     when    = destroy
     command = "multipass delete ${self.triggers.name} --purge"
